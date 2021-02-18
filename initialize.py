fefb93ef10f37c2d9cee18207b2c8c8271b0eb44
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-02-18 10:06:20 +0700

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -1,13 +1,12 @@
-def initialize():
-  """
-  Initialize simulator
-  Install Octave and clone repos, takes ~3-5 mins.
-  """
-  # Install Octave
-  !apt install -q octave
+"""
+Initialize simulator
+Install Octave and clone repos, takes ~3-5 mins.
+"""
+# Install Octave
+!apt install -q octave
 
-  # Cloning repositories
-  !git clone https://bitbucket.org/mrst/mrst-core.git
-  !git clone https://yohanesnuwara@bitbucket.org/mrst/mrst-autodiff.git
-  !git clone https://github.com/yohanesnuwara/reservoir_datasets
-  !git clone https://github.com/yohanesnuwara/pyMRST
+# Cloning repositories
+!git clone https://bitbucket.org/mrst/mrst-core.git
+!git clone https://yohanesnuwara@bitbucket.org/mrst/mrst-autodiff.git
+!git clone https://github.com/yohanesnuwara/reservoir_datasets
+!git clone https://github.com/yohanesnuwara/pyMRST
