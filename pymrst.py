19abe0117b7e4d8c3e1d9600f6f17352cf3a6612
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-02-28 11:41:53 +0700

    add setup function

---
+++ pymrst.py
@@ -0,0 +1,14 @@
+def setup():
+  import subprocess
+
+  def install(name):
+    subprocess.call(['apt', 'install', name])
+  
+  def clone(url):
+    subprocess.call(['git', 'clone', url])
+  
+  install('octave')
+  clone('https://bitbucket.org/mrst/mrst-core.git')
+  clone('https://bitbucket.org/mrst/mrst-autodiff.git')
+  clone('https://github.com/yohanesnuwara/reservoir_datasets')
+  clone('https://github.com/yohanesnuwara/pyMRST')
