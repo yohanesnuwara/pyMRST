b28c6b787af5f68e5cb3f30856060cd4dce6a9e8
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2021-02-28 11:51:54 +0700

    update setup

--- pymrst.py
+++ pymrst.py
@@ -7,8 +7,13 @@ def setup():
   def clone(url):
     subprocess.call(['git', 'clone', url])
   
+  # Install octave and cloning repositories
   install('octave')
   clone('https://bitbucket.org/mrst/mrst-core.git')
   clone('https://bitbucket.org/mrst/mrst-autodiff.git')
   clone('https://github.com/yohanesnuwara/reservoir_datasets')
   clone('https://github.com/yohanesnuwara/pyMRST')
+  
+  # Add system path
+  import sys
+  sys.path.append("/content/pymrst")  
